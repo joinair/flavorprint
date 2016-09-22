c9ab0079e9eb265b571783b0c1874b9148455436
Maxim Kostuchenko <kstmaks@gmail.com>
2016-09-22 17:53:30 +0500

    Cleanup routes

--- src/constants/Routes.js
+++ src/constants/Routes.js
@@ -1,61 +1,18 @@
 
-export const COOKBOOK = '/cookbook';
 export const LOG_IN = '/login';
-export const ONLINE_CHECKOUT = '/online-checkout';
 export const PREFERENCES = '/preferences';
-export const PRINT_RECIPE = '/print-recipe';
-export const PRINT_SHOPPING_LIST = '/print-shopping-list';
-export const RECIPE_FEED = '/';
+export const RECIPES = '/recipes';
 export const RESET_PASSWORD = '/reset-password';
-export const SEARCH_RESULTS = '/search';
 export const SETTINGS = '/settings';
 export const SETTINGS_MORE = '/settings-more';
-export const SHOPPING_LIST = '/shopping-list';
 export const SIGN_UP = '/signup';
 
-export const RECIPES_NEW = '/recipes/new';
-export const RECIPES_IMPORT = '/recipes/new/import';
-export const RECIPES_FROM_PARTNERS = '/recipes/from-partners';
-
-export const MOBILE_SPLASH = '/mobile-splash';
-export const MOBILE_FOR_YOU = '/mobile-for-you';
-export const MOBILE_SEARCH = '/mobile-search';
-export const MOBILE_COOKBOOK = '/mobile-cookbook';
-export const MOBILE_SETTINGS = '/mobile-settings';
-export const MOBILE_FILTERS = '/mobile-filters';
-export const MOBILE_SHOPPING_LIST = '/mobile-shopping-list';
-export const MOBILE_WEB_VIEW = '/mobile-web-view';
-export const MOBILE_AVOIDANCES_MODAL = '/mobile-avoidances-modal';
-export const MOBILE_WELCOME = '/mobile-welcome';
-export const MOBILE_SHOPPING_LIST_ITEMS = '/mobile-shopping-list-items';
-export const MOBILE_SEARCH_QUERY = '/mobile-search-query';
-export const MOBILE_SEARCH_FILTERS_INGREDIENTS = '/mobile-search-filters-ingredients';
-export const MOBILE_COLLECTIONS_MODAL = '/mobile-collections-modal';
-
 export default {
-  COOKBOOK,
   LOG_IN,
-  ONLINE_CHECKOUT,
   PREFERENCES,
-  PRINT_RECIPE,
-  PRINT_SHOPPING_LIST,
-  RECIPE_FEED,
+  RECIPES,
   RESET_PASSWORD,
-  SEARCH_RESULTS,
   SETTINGS,
-  SHOPPING_LIST,
+  SETTINGS_MORE,
   SIGN_UP,
-  RECIPES_NEW,
-  RECIPES_IMPORT,
-
-  MOBILE_FOR_YOU,
-  MOBILE_SEARCH,
-  MOBILE_COOKBOOK,
-  MOBILE_SETTINGS,
-  MOBILE_FILTERS,
-  MOBILE_WELCOME,
-  MOBILE_AVOIDANCES_MODAL,
-  MOBILE_SHOPPING_LIST_ITEMS,
-  MOBILE_SEARCH_QUERY,
-  MOBILE_COLLECTIONS_MODAL,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,17 +6,11 @@ import preferences from 'actions/preferences';
 import { anonymousSignUp } from 'actions/user';
 
 import authenticationRoutes from './authentication';
-import cookbookRoutes from './cookbook';
 import homeRoute from './home';
 import notFound from './notFound';
-import printRecipeRoute from './printRecipe';
-import printShoppingListRoute from './printShoppingList';
-import recipesRoutes from './recipes';
+import recipesRoute from './recipes';
 import resetPasswordRoute from './resetPassword';
-import searchResults from './searchResults';
 import settingsRoutes from './settings';
-import shoppingListRoute from './shoppingList';
-import onlineCheckoutRoute from './onlineCheckout';
 
 import OAuthCallbackRoute from './OAuthCallback';
 
@@ -37,22 +31,15 @@ export default store => [
     },
 
     childRoutes: [
-      printShoppingListRoute(store),
-      printRecipeRoute(store),
-
       {
         component: Layout,
         childRoutes: [
           ...authenticationRoutes(store),
           homeRoute(store),
-          ...recipesRoutes(store),
+          recipesRoute(store),
           resetPasswordRoute(store),
-          searchResults(store),
           ...settingsRoutes(store),
-          shoppingListRoute(store),
-          onlineCheckoutRoute(store),
 
-          ...cookbookRoutes(store), // WARNING: this route must be penultimate
           notFound(store), // WARNING: this route must be last
         ],
       },
