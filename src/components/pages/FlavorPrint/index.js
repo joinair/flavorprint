74b77a65d9de7de40dbc273329a922264ec0c07c
Max Kostuchenko <kstmaks@gmail.com>
2016-10-18 03:03:35 +0500

    Static content

--- src/components/pages/FlavorPrint/index.js
+++ src/components/pages/FlavorPrint/index.js
@@ -2,12 +2,14 @@
 import React, { PropTypes } from 'react';
 
 import UserData from './UserData';
+import StaticContent from './StaticContent';
 
 import './styles.css';
 
 const FlavorPrint = () => (
   <div className="FlavorPrint">
     <UserData />
+    <StaticContent />
   </div>
 );
 
